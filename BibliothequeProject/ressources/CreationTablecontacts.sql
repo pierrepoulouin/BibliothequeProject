1a4843e822d17fa55dc2f3ebb01217b90f066041
pierrepoulouin <pierrepoulouin@gmail.com>
2018-08-28 19:38:41 +0200

    Creation Table

---
+++ BibliothequeProject/ressources/CreationTablecontacts.sql
@@ -0,0 +1,6 @@
+CREATE TABLE contacts (
+nom VARCHAR(60),
+prenom VARCHAR(60),
+email VARCHAR(80),
+telephone VARCHAR (20))
+;
---
+++ BibliothequeProject/ressources/CreationTableinscriptions.sql
@@ -0,0 +1,6 @@
+CREATE TABLE inscriptions (
+nom VARCHAR(60),
+prenom VARCHAR(60),
+email VARCHAR(80),
+password VARCHAR(50)
+);
