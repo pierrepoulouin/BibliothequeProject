1703030cd5b0be0e7f80cac5f4918b5485bbdbaf
pierrepoulouin <pierrepoulouin@gmail.com>
2018-08-28 19:38:53 +0200

    Insertion des infos

---
+++ BibliothequeProject/ressources/InsererInfoLivre.sql
@@ -0,0 +1,9 @@
+INSERT INTO `bibliotheque`.`livres`
+(`titre`,
+`auteur`,
+`editeur`)
+VALUES
+('Da Vinci Code', 'Dane Brown', 'Galimard'),
+('Le vilain petit canard', 'LeFadi', 'Simplon'),
+('Le vilain, la brute et le truand', 'Victor Hugo', 'JC Laccès'),
+('Tirlipimpon', 'Pompidou' 'Simplon')
